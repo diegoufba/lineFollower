2070d5f4c7df07f35a13de5518887021acdcd8e4
Felipe Goes <felipecgoes4@gmail.com>
2024-12-16 23:54:26 -0300

    fix PDI and PD not final and added grafico erro medio

--- controllers/line_follower/line_follower.py
+++ controllers/line_follower/line_follower.py
@@ -1,4 +1,6 @@
 from controller import Robot
+import numpy as np
+import matplotlib.pyplot as plt
 
 def run_robot(robot):
     time_step = 32
@@ -26,10 +28,16 @@ def run_robot(robot):
     right_ir1.enable(time_step)
     right_ir2 = robot.getDevice('ir2right')
     right_ir2.enable(time_step)
-     
+    
+    graph1Timing = 0
+    graph1GenerationTiming = 0
+    aux_erros = []
+    aux_times = 1
+    
     erro_anterior = 0
     integral_erro = 0
     limite_integral = 10
+    
     #CONSTANTES DOS CONTROLADORES
     kd_PD = 1
     kp_PD = 1
@@ -37,9 +45,14 @@ def run_robot(robot):
     kd_PDI = 1
     kp_PDI = 1
     ki_PDI = 0.1
+    
+    #VETORES DOS GRAFICOS
+    x_graph_erro_medio = []
+    y_graph_erro_medio = []
 
     # Step simulation
     while robot.step(time_step) != -1:
+        
         # read ir sensors
         left_ir0_value = round(left_ir0.getValue(),2)
         left_ir1_value = round(left_ir1.getValue(), 2)
@@ -52,27 +65,64 @@ def run_robot(robot):
         print(f"left: [{left_ir0_value}, {left_ir1_value}, {left_ir2_value}] right: [{right_ir0_value}, {right_ir1_value}, {right_ir2_value}]")
         
         #NORMALIZAÇÃO - PORQUE DESSES VALORES??? -> Investigar
-        left_ir0_value = 1 if left_ir0_value< 5 else 0 
-        left_ir1_value = 1 if left_ir1_value< 5 else 0
-        left_ir2_value = 1 if left_ir2_value< 5 else 0
+        #ABSTRACAO APLICADA: 0 significa não detecta a linha preta e 1 significa detecta a linha preta em vez de representar as cores
+        left_ir0_value = 0 if left_ir0_value< 5 else 1 
+        left_ir1_value = 0 if left_ir1_value< 5 else 1
+        left_ir2_value = 0 if left_ir2_value< 5 else 1
         
-        right_ir0_value = 1 if right_ir0_value< 5 else 0 
-        right_ir1_value = 1 if right_ir1_value< 5 else 0 
-        right_ir2_value = 1 if right_ir2_value< 5 else 0 
+        right_ir0_value = 0 if right_ir0_value< 5 else 1 
+        right_ir1_value = 0 if right_ir1_value< 5 else 1 
+        right_ir2_value = 0 if right_ir2_value< 5 else 1 
         
         print(f"-Valores normalizados: left: [{left_ir0_value}, {left_ir1_value}, {left_ir2_value}] right: [{right_ir0_value}, {right_ir1_value}, {right_ir2_value}]")
-        erro = (left_ir0_value + left_ir1_value + left_ir2_value) - (right_ir0_value + right_ir1_value + right_ir2_value)
+        #erro = (left_ir0_value + left_ir1_value + left_ir2_value) - (right_ir0_value + right_ir1_value + right_ir2_value)
         #OU - Linha é pequena, nunca vai estar 2 ao mesmo tempo
-        #erro = (left_ir0_value + 2*left_ir1_value + 3*left_ir2_value) - (right_ir0_value + 2*right_ir1_value + 3*right_ir2_value)
+        erro = (left_ir0_value + 2*left_ir1_value + 3*left_ir2_value) - (right_ir0_value + 2*right_ir1_value + 3*right_ir2_value)
         
+        if graph1Timing >= time_step*50: #Registra o erro medio apos 50 time_steps, 1.6 segundos
+               
+            #print(aux_erros)
+            #print(f"--ERRO MEDIO: {np.mean(aux_erros)}")
+            x_graph_erro_medio.append(1.6*aux_times)
+            y_graph_erro_medio.append(np.mean(aux_erros))
+            
+            aux_erros = []
+            aux_times += 1
+            graph1Timing = 0
+            
+        else:
+            aux_erros.append(erro) #acumula o erro nos 50 time steps
+            graph1Timing += time_step
+            
+        if graph1GenerationTiming >= 4000*time_step: #gera um grafico a cada pouco mais que 2 minutos, uma volta completa e mais um pouco
+            fig, ax = plt.subplots()
+            ax.set_xlim([0, 120])
+            ax.set_ylim([-0.5, 0.5])
+            ax.set_title(f"Erro Médio X Tempo")
+            ax.set_xlabel("Tempo (s)")
+            ax.set_ylabel("Erro Médio")
+            
+            ax.axvline(x=110, color='red', linestyle='--', label="x = 110") # Adiciona a linha vertical em x=3450, quando aproximadamente completa uma volta          
+           
+            graph = ax.plot(x_graph_erro_medio, y_graph_erro_medio, color='black')[0]
+            plt.show()
+            
+            graph1GenerationTiming = 0
+            
+        else:
+            graph1GenerationTiming += time_step
+           
+              
         #PD
         #derivada_erro = (erro - erro_anterior)/time_step
         #controle = erro*kp_PD + derivada_erro*kd_PD
         
-        #if controle > 0: #Está virando para a esquerda ajustar para a direita
-            #left_motor.setVelocity(max_speed+controle)
-            #right_motor.setVelocity(max_speed-controle)
-        #elif controle < 0: #Está virando para a direita ajustar para a esquerda
+        #print(f"---CONTROLE: {controle}")
+        
+        #if controle > 0: #Significa que os sensores da esquerda do robô estão sobre a faixa, está virando para a direita ajustar para a esquerda
+            #left_motor.setVelocity(max_speed-controle)
+            #right_motor.setVelocity(max_speed+controle)
+        #elif controle < 0: #Significa que os sensores da direita do robô estão sobre a faixa, o robo está virando para a esquerda ajustar para a direita
             #left_motor.setVelocity(max_speed-controle)
             #right_motor.setVelocity(max_speed+controle)
         #else:
@@ -95,21 +145,25 @@ def run_robot(robot):
         if erro == 0: 
             integral_erro *= 0.7
             
+        
         controle = erro*kp_PDI + derivada_erro*kd_PDI + integral_erro*ki_PDI
+        print(f"---CONTROLE: {controle}")
             
-        if controle > 0: #Está virando para a esquerda ajustar para a direita
-            left_motor.setVelocity(max_speed+controle)
-            right_motor.setVelocity(max_speed-controle)
-        elif controle < 0: #Está virando para a direita ajustar para a esquerda
+        if controle > 0: #Significa que os sensores da esquerda do robô estão sobre a faixa, está virando para a direita ajustar para a esquerda
+            left_motor.setVelocity(max_speed-controle)
+            right_motor.setVelocity(max_speed+controle)
+        elif controle < 0: #Significa que os sensores da direita do robô estão sobre a faixa, o robo está virando para a esquerda ajustar para a direita
             left_motor.setVelocity(max_speed-controle)
             right_motor.setVelocity(max_speed+controle)
         else:
             left_motor.setVelocity(max_speed)
             right_motor.setVelocity(max_speed)
         
-        print(f"--ERROR: {erro}")
+        #print(f"--ERROR: {erro}")
         
         erro_anterior = erro   
+        
+        
 
 if __name__ == "__main__":
     my_robot = Robot()
